da6067e5104acd7baf2f369897583ae3a55f5468
DifexRV <roOmkin@gmail.com>
2022-11-10 17:45:38 +0700

    Рабочая ветка для ДЗ - Курсовая работа 1-го курса.

---
+++ src/Pro/sky/EmployeeBook.java
@@ -0,0 +1,19 @@
+package Pro.sky;
+
+public class EmployeeBook {
+
+    public static Employee[] employees = new Employee[]{
+            new Employee(3, "Тролль Борис Иванович", 23000),
+            new Employee(2, "Кранова Матильда Боссовна", 22000),
+            new Employee(4, "Бристоль Матеос Кратосович", 21000),
+            new Employee(2, "Врунишкин Аркадий Мамонтович", 20000),
+            new Employee(4, "Медлеходов Бронислав Грузович", 27000),
+            new Employee(1, "Наглышов Глав Главнович", 67000),
+            new Employee(3, "Грустный Олег Станиславович", 17000),
+            new Employee(1, "Горгона Медуза Форкиевна", 64000),
+            new Employee(1, "Привередов Виктор Графович", 62500),
+            new Employee(5, "Простофилин Иван Иванович", 17500),
+    };
+
+
+}
